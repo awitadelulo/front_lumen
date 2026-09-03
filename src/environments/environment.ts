29841f49ad00453968c0e20341f7ab1ee93f11e6
David <david.lopez.rivera.2003@gmail.com>
2026-09-03 01:07:28 -0500

    add public url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Cuando despliegues el FastAPI, reemplaza esto por la URL pública (ej: https://api.lumenlegal.com/api/v1)
-  apiUrl: 'https://TU-API-DESPLEGADA.example.com/api/v1',
+  apiUrl: 'https://backend-lu.onrender.com/api/v1',
 };
